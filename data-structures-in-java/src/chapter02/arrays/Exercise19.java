a74fcb3261fdb5288fcddfc31b7f398ce92a958c
Leo <Leo@192.168.2.154>
2017-05-30 15:58:36 -0400

    started the package for chapter 02 > arrays in java

---
+++ data-structures-in-java/src/chapter02/arrays/Exercise19.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package chapter02.arrays;
+
+/** Exercise 2.19
+ * Data Structures with Java, Second Edition, John R. Hubbard, 
+ * Schaum’s Outline Series
+ * @author Leo Nataj
+ * The Sieve of Eratosthenes is an array of boolean elements whose ith element is true if and
+ * only if i is a prime number. 
+ * Use the following algorithm to compute and print a sieve of size 1000:
+ * 1. Initialize p[0] and p[1] to be false, and all other p[i] to be true.
+ * 2. Repeat step 3 for each i from 3 to n, incrementing by 2.
+ * 3. If there is a prime the square root of i that divides i, set p[i] false.
+ */
+public class Exercise19 {
+
+}
