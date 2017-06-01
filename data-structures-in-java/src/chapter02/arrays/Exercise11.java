239e1ac25288bd057a072471e6d6a738c2e93147
Leo <Leo@192.168.2.154>
2017-06-01 09:22:11 -0400

    made a generic method to concat arrays

--- data-structures-in-java/src/chapter02/arrays/Exercise11.java
+++ data-structures-in-java/src/chapter02/arrays/Exercise11.java
@@ -3,8 +3,10 @@
  */
 package chapter02.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.*;
+import java.util.Collections;
+
 
 /**  Exercise 2.11
  * @author Leo Nataj
@@ -18,19 +20,33 @@ public class Exercise11 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// testInteger();
+		String[] x = {"She", "is", "mad"};
+		String[] y = {"But", "it", "is", "Alright"};
+		String[] result = concatenate(x, y);
+		System.out.println(Arrays.toString(result));
+		
+	}
+	
+	
+	
+	
+	public static void testInteger() {
 		Integer[] x = {2, 4, 3, 5};
 		Integer[] y = {-2, 14, 3, 5};
 		Integer[] result = concatenate(x, y);
-		
-		System.out.println(Arrays.asList(result));
-
+		System.out.println(Arrays.toString(result));
 	}
 	/** returns an array containing all of a[] 
 	 * followed by all of b[]
 	 */
-	public static Integer[] concatenate(Integer[] a, Integer[] b) {
-		Stream<Integer> aa = Stream.of(a);
-		Stream<Integer> bb = Stream.of(b);
-		return Stream.concat(aa, bb).toArray(Integer[]::new);
-	}
+	@SafeVarargs
+	public static <T> T[] concatenate(T[]... arrays) {
+        ArrayList<T> result = new ArrayList<T>();
+        for (T[] x : arrays)
+            Collections.addAll(result, x);
+        return (T[]) result.toArray(arrays[0].clone());
+    }
+	
+	
 }
