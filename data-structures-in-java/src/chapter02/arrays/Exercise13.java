b41aa1ab1f1c6aded21f9808b94f2fc2e6725830
Leo <Leo@192.168.2.154>
2017-06-05 00:04:02 -0400

    generated the array

--- data-structures-in-java/src/chapter02/arrays/Exercise13.java
+++ data-structures-in-java/src/chapter02/arrays/Exercise13.java
@@ -2,6 +2,7 @@
  * 
  */
 package chapter02.arrays;
+import java.util.Arrays;
 import java.util.stream.*;
 
 /**  Exercise 2.13
@@ -13,17 +14,21 @@ import java.util.stream.*;
 public class Exercise13 {
 
 	public static void main(String[] args) {
-		System.out.println(tally("abda"));
+		System.out.println(Arrays.toString(tally("abda")));
 
 	}
 	
 	/** returns an array a[] of 26 integers that count the frequencies
 	* of the (case insensitive) letters in the given string
 	*/
-	public static int tally(String string) {
-		
-		int num  = (int) string.chars().filter(s -> s == 'a').count();
-		return num;
+	public static int[] tally(String string) {
+		char[] charArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		int[] result = new int[26];
+		for (int i=0; i<26; i++) {
+			char ch = charArray[i];
+			result[i] = (int) string.chars().filter(s -> s == ch).count();
+		}
+		return result;
 	}
 
 }
