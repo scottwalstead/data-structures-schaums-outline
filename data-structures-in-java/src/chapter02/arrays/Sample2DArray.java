67ccb80ad812079020dc78428219e3879773c73d
Leo <Leo@192.168.2.154>
2017-06-06 21:32:30 -0400

    relocate the initialization of array inside the constructor

--- data-structures-in-java/src/chapter02/arrays/Sample2DArray.java
+++ data-structures-in-java/src/chapter02/arrays/Sample2DArray.java
@@ -1,11 +1,10 @@
-/**
- * 
- */
+
 package chapter02.arrays;
 
 import java.util.Random;
 
-/**  Generates a sample 2D Rectangular 2D Array 
+/**  Generates a sample 2D Rectangular 2D Array
+ * dimensions of array are given as row and col 
  * @author Leo Nataj
  * Data Structures with Java, Second Edition, John R. Hubbard,
  * Schaum’s Outline Series
@@ -15,25 +14,30 @@ public class Sample2DArray {
 
 	private int row;
 	private int col;
+	private int[][] arr; // declaration
+	
 	public Sample2DArray(int row, int col) {
 		this.row = row;
 		this.col = col;
+		this.arr = new int[row][col]; // initialize the array
+		Random ran = new Random();
+		// allocating the elements
+		for (int i = 0; i < row; i++) { 
+			for (int j = 0; j < col; j++) { 
+				arr[i][j] = ran.nextInt(10); 
+			} 
+		}
 	}
 	
-	public int[][] getArray() {
-		Random ran = new Random(10);
-		int[][] arr = new int[row][col];
-		for (int i=0; i<row; i++) {
-			for (int j=0; j<col; j++){
-				arr[i][j] = ran.nextInt();
-			}
-		}
+	
+	public int[][] getArr() {
 		return arr;
 	}
-	
-	public void display(int[][] arr) {
-		for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
+
+
+	public void display() {
+		for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
                 System.out.print(arr[i][j]+" "); 
             }
             System.out.println("");
---
+++ data-structures-in-java/src/chapter02/arrays/TestSampleArray.java
@@ -0,0 +1,11 @@
+package chapter02.arrays;
+
+public class TestSampleArray {
+
+	public static void main(String[] args) {
+		Sample2DArray a = new Sample2DArray(3, 4);
+		a.display();
+
+	}
+
+}
