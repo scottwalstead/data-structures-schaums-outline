1559d21468610cae5cd4cb04eaacd99187563d29
Leo Nataj <nattaj@gmail.com>
2017-06-07 13:05:51 -0400

    Pascal triangle

---
+++ data-structures-in-java/src/chapter02/arrays/Exercise18.java
@@ -0,0 +1,22 @@
+package chapter02.arrays;
+
+/**  Exercise 2.18
+ * @author Leo Nataj
+ * Data Structures with Java, Second Edition, John R. Hubbard,
+ * Schaum’s Outline Series
+ */
+
+public class Exercise18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/** returns Pascal’s triangle of the given size
+	*/
+	public static int[][] pascal(int size) {
+		
+	}
+	
+}
