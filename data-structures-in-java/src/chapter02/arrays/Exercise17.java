15bc3e81707efcfc488d7db9a17c30fc2468e38b
Leo Nataj <nattaj@gmail.com>
2017-06-07 12:22:07 -0400

    transpose of a 2D array

---
+++ data-structures-in-java/src/chapter02/arrays/Exercise17.java
@@ -0,0 +1,32 @@
+package chapter02.arrays;
+
+
+/**  Exercise 2.14
+ * @author Leo Nataj
+ * Data Structures with Java, Second Edition, John R. Hubbard,
+ * Schaum’s Outline Series
+ */
+
+
+public class Exercise17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/** returns the transpose of 
+	 * the specified array as a matrix:
+	 * ta[i][j] = a[j][i]
+	 */
+	public static double[][] transpose(double[][] a) {
+		double[][] ta = new double[a[0].length][a.length];
+		for (int i=0; i<a.length; i++) {
+			for (int j=0; j<a[0].length; j++) {
+				ta[i][j] = a[j][i];
+			}
+		}
+		return ta;
+	}
+	
+}
