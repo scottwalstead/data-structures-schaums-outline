c2d37509f115110e5f2dc4392e8f6ac2e14bb40f
Leo Nataj <nattaj@gmail.com>
2017-06-10 22:22:45 -0400

    Defined method for calculating each element

--- data-structures-in-java/src/chapter02/arrays/Exercise18.java
+++ data-structures-in-java/src/chapter02/arrays/Exercise18.java
@@ -1,5 +1,8 @@
 package chapter02.arrays;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**  Exercise 2.18
  * @author Leo Nataj
  * Data Structures with Java, Second Edition, John R. Hubbard,
@@ -9,14 +12,28 @@ package chapter02.arrays;
 public class Exercise18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] prevRow = {1, 5, 10, 10, 5, 1};
+		IntStream.range(0,7).forEach(i -> System.out.println(calcElement(i, prevRow)));
 	}
 
 	/** returns Pascal’s triangle of the given size
-	*/
-	public static int[][] pascal(int size) {
-		
-	}
 	
-}
+	public static int[][] pascal(int size) {
+	/** int[][] result = new int[size][size];
+	Stream.builder().
+	for (int i=1; i<size; i++) {	
+		int IntStream.range(0, i).map(i -> result[i-1][] - z[i]).toArray();
+	}
+	} */
+	
+	public static int calcElement(int index, int[] prevRow) {
+		if (index>0 && index<prevRow.length) {
+			return (prevRow[index-1] + prevRow[index]);
+		} else {
+			return 1;
+		}
+		
+			
+		}
+	}
+
