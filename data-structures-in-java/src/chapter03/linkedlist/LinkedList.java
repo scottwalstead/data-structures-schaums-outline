2848d118740ffbaf25caaa55f798e037feabc614
Leo Nataj <nattaj@gmail.com>
2017-06-23 09:20:27 -0400

    Node nested class created

---
+++ data-structures-in-java/src/chapter03/linkedlist/LinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package chapter03.linkedlist;
+
+/**
+ * @author Mr. Leo Nataj
+  * Data Structures with Java, Second Edition, John R. Hubbard,
+ * Schaum’s Outline Series
+ */
+
+public class LinkedList {
+
+	private Node start;
+
+	public void insert(int x) {
+		// Insert lines 2-14 of Example 3.12 on page 58
+	}
+
+	public void delete(int x) {
+		// Insert lines 2-18 of Example 3.13 on page 59
+	}
+	private static class Node {
+		// Insert lines 2-12 of Example 3.10 on page 56
+	}
+}
